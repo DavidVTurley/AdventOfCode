8feaa926e0c19e35752160235614a8f6ba3899bb
David <david.turley@live.nl>
2021-12-14 00:12:08 +0100

    2021 Challenge 3 Wip

---
+++ AdventOfCode/AdventOfCode/Benchmark/tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+
+namespace AdventOfCode.Benchmark
+{
+    public class TestSuite
+    {
+        
+        public List<String> TestSquential(String value)
+        {
+            return value.Split(',').ToList();
+        }
+
+        public List<String> TestParralel(String value)
+        {
+            return value
+            .Split(',')
+            .AsParallel()
+            .ToList();
+        }
+
+    }
+}
--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -1,13 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode._2021;
+using AdventOfCode.Benchmark;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.Diagnostics.Tracing.Parsers.MicrosoftAntimalwareEngine;
 
 
-
-
-
-
-
-
-_02_SubmarineHorizontalMovement program = new();
+_03_SubmarineDiagnostics program = new();
 program.Run();
 program.RunSecond();
+
+BenchmarkRunner.Run<Benchmarks>();
+
+
